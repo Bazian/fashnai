dd9dabfc9c7d88304db4b7acd6957a9c202cbdc1
Nithya <github@nmogane.net>
2026-02-09 21:55:47 +0100

    Fix Next.js static export: use output config instead of deprecated next export command

---
+++ frontend/app/product/[url]/generateStaticParams.js
@@ -0,0 +1,6 @@
+// Generate static params for product pages
+export async function generateStaticParams() {
+  // Return empty array to generate no static pages
+  // All product pages will be rendered on-demand
+  return [];
+}
